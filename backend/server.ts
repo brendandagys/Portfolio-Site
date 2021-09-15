d6a3df64815d73797baf75ea14c6ff92a1804dc1
Brendan Dagys <brendandagys@gmail.com>
2021-09-14 21:35:08 -0400

    Add backend Dockerfile and minor server.ts changes

--- backend/server.ts
+++ backend/server.ts
@@ -13,6 +13,10 @@ if (process.env.NODE_ENV === 'production') {
   // Express will serve production assets like our main.js or main.css file
   app.use(express.static(path.join(process.cwd(), '..', 'frontend', 'build')))
 
+  app.get('/health', (req: Request, res: Response) => {
+    res.send('Healthy!')
+  })
+
   // Express will serve up the index.html file if it doesn't recognize the route
   app.get('*', (req: Request, res: Response) => {
     res.sendFile(
@@ -22,10 +26,6 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   app.get('/', (req: Request, res: Response) => {
     res.send('API server is running...')
-  })
-
-  app.get('/health', (req: Request, res: Response) => {
-    res.send('Healthy!')
   })
 }
 
