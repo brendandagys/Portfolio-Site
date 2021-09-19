23747b52c0621470d4afdfee7eae65392722d698
Brendan Dagys <brendandagys@gmail.com>
2021-09-18 21:00:19 -0400

    Many Dockerfile and docker-compose.yml updates

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,12 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const EXPRESS_HOST = process.env.EXPRESS_HOST ?? 'localhost'
+const PORT = process.env.BACKEND_PORT ?? '5000'
 
 module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: `http://${EXPRESS_HOST}:5000`,
+      target: `http://${EXPRESS_HOST}:${PORT}`,
       changeOrigin: true,
     })
   )
