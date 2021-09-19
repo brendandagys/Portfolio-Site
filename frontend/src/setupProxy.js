bf3eed50c00c9b51cd1f414c2dab8d74f6e6083d
Brendan Dagys <brendandagys@gmail.com>
2021-09-19 15:45:52 -0400

    Update docker-compose ports back to 3000 and 5000

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,13 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const EXPRESS_HOST = process.env.EXPRESS_HOST ?? 'localhost'
-const PORT = process.env.BACKEND_PORT ?? '5000'
 
 module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: `http://${EXPRESS_HOST}:${PORT}`,
+      target: `http://${EXPRESS_HOST}:5000`,
       changeOrigin: true,
     })
   )
