6ed8f0608b2f5523d4ada07af7e14fab9f0c816a
Brendan Dagys <brendandagys@gmail.com>
2021-09-25 10:43:31 -0400

    Update axios.ts to use http rather than https and improve create-react-app.yml

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -4,7 +4,7 @@ let axiosNewBaseURL: AxiosInstance
 
 if (process.env.NODE_ENV === 'production') {
   axiosNewBaseURL = axios.create({
-    baseURL: `${window.location.href.split('://')[0]}://52.60.182.165:8080`,
+    baseURL: 'http://52.60.182.165:8080',
   })
 } else {
   axiosNewBaseURL = axios
