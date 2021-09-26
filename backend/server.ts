e172331f823c6d9e0eb686e3c11dfc8876469ce9
Brendan Dagys <brendandagys@gmail.com>
2021-09-26 12:43:31 -0400

    Nearing completion of infrastructure set-up...

--- backend/server.ts
+++ backend/server.ts
@@ -1,22 +1,13 @@
 import express from 'express'
 import { Request, Response } from 'express'
 
-import https from 'https'
-import fs from 'fs'
-
-const key = fs.readFileSync('/usr/src/backend/dist/key.pem')
-const cert = fs.readFileSync('/usr/src/backend/dist/cert.pem')
-
-// import path from 'path'
 import dotenv from 'dotenv'
 
 dotenv.config({ path: __dirname + (process.env.ENVPATH ?? '/../.env') })
 
-const PORT = 8080
+const PORT = 80
 
 const app = express()
-
-const server = https.createServer({ key: key, cert: cert }, app)
 
 app.use((req, res, next) => {
   res.set({
@@ -32,6 +23,16 @@ app.get('/api/health', (req: Request, res: Response) => {
   res.send('Healthy!')
 })
 
+app.get('*', (req: Request, res: Response) => {
+  res.send(`API server running in ${process.env.NODE_ENV}...`)
+})
+
+app.listen(PORT, () =>
+  console.log(
+    `API server running in ${process.env.NODE_ENV} on port ${PORT}...`
+  )
+)
+
 // if (process.env.NODE_ENV === 'production') {
 //   // Express will serve production assets like our main.js or main.css file
 //   app.use(express.static(path.join(process.cwd(), '..', 'frontend', 'build')))
@@ -43,13 +44,3 @@ app.get('/api/health', (req: Request, res: Response) => {
 //     )
 //   })
 // }
-
-app.get('*', (req: Request, res: Response) => {
-  res.send(`API server running in ${process.env.NODE_ENV}...`)
-})
-
-server.listen(PORT, () =>
-  console.log(
-    `API server running in ${process.env.NODE_ENV} on port ${PORT}...`
-  )
-)
