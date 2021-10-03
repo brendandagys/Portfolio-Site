a62d888650146c46adcca822109ed565c746d860
Brendan Dagys <brendandagys@gmail.com>
2021-10-02 21:08:48 -0400

    Essentially done CloudFormation template

--- backend/server.ts
+++ backend/server.ts
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(express.json())
 
 app.get('/api/health', (req: Request, res: Response) => {
-  res.send('Healthy!')
+  res.send('API server is healthy!')
 })
 
 app.get('*', (req: Request, res: Response) => {
