bb90ebadf08fbb6fd93e0bf0418329f436bedb11
Brendan Dagys <brendandagys@gmail.com>
2021-10-03 11:35:10 -0400

    Latest updates

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,4 @@
-import express from 'express'
-import { Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 
 import dotenv from 'dotenv'
 dotenv.config({ path: __dirname + (process.env.ENVPATH ?? '/../.env') })
