319509d295a723f461335cedbcda2c6e8a1a8fa4
Brendan Dagys <brendandagys@gmail.com>
2021-12-31 11:27:55 -0500

    Ensure adequate padding of 'Thank You' modal component

--- frontend/src/components/ContactThankYouModal.tsx
+++ frontend/src/components/ContactThankYouModal.tsx
@@ -5,7 +5,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 340,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -26,7 +26,7 @@ const MyModal = ({ open, handleClose }: MyModalProps) => {
         aria-labelledby='modal-modal-title'
         aria-describedby='modal-modal-description'
       >
-        <Box sx={style}>
+        <Box sx={style} style={{ textAlign: 'center' }}>
           <Typography id='modal-modal-title' variant='h6' component='h2'>
             Thank you!
           </Typography>
