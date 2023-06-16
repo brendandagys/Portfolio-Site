7360504e71c49f9c54daf1bf79152518f1315f23
Brendan Dagys <brendandagys@gmail.com>
2023-06-15 20:20:22 -0400

    Update accordion content

--- frontend/src/data/accordionContent.tsx
+++ frontend/src/data/accordionContent.tsx
@@ -8,48 +8,60 @@ const StyledTypography = styled(Typography)`
   // text-align: justify;
 `
 
-const StyledListItem = styled.li`
-  margin-top: -12px;
-`
-
 export const accordionContentBackground = (
   <>
     <StyledTypography>
-      I live in Toronto, Ontario, Canada and graduated from Ryerson University
-      with a Bachelor’s degree in Biomedical Engineering. After working for a
-      few years as an engineer, I developed a passion for coding, data analysis,
-      and machine learning. Although we learned most of these skills in
-      university, I chose to further my education in these areas and apply it to
-      the field of healthcare, which I am passionate about. Since then, I have
-      worked at two large Ontario hospitals in data-centric roles.
+      I live in Toronto, Ontario, Canada and graduated from Ryerson University with
+      a Bachelor’s degree in Biomedical Engineering. After working for a few years
+      as an engineer, I developed a passion for coding, data analysis, and machine
+      learning. Although we learned most of these skills in university, I chose at
+      the time to further my education in these areas and apply it to the field of
+      healthcare. This resulted in me working in two roles at two large Ontario
+      hospitals between 2018 and 2021.
     </StyledTypography>
     <StyledTypography>
-      At these organizations, I was able to identify many processes that were
-      ripe for improvement and apply software solutions to fix them. This
-      involved the automation of numerous tasks, such as data retrieval
-      (databases, files, FTP servers, etc.), data cleaning/munging, and
-      dissemination of data and analyses (emails, file folders, applications,
-      etc.). This skillset improved the way that the organizations utilized
-      their data holdings, dashboards, and various reports that come from
-      multifarious internal and external systems.
+      At these organizations, I was able to identify many processes that were ripe
+      for improvement and apply software solutions to fix them. This involved the
+      automation of numerous tasks, such as data retrieval (databases, files, FTP
+      servers, etc.), data cleaning/munging, and dissemination of data and
+      analyses (emails, file folders, applications, etc.). This skillset improved
+      the way that the organizations utilized their data holdings, dashboards, and
+      various reports that come from multifarious internal and external systems.
     </StyledTypography>
     <StyledTypography>
       I was also able to apply my skillset toward the development of Web
-      applications, building applications that solved problems at a larger
-      scale. I created tools for work but also numerous Websites as personal
-      projects for my own interest and edification. I designed these projects
-      not only to solve real problems, but also to work on specific areas of
-      software development with the goal of becoming a well-rounded full-stack
-      developer.
+      applications, building applications that solved problems at a larger scale.
+      I created tools for work but also numerous websites as personal projects for
+      my own interest and edification. I designed these projects not only to solve
+      real problems, but also to work on specific areas of software development
+      with the goal of becoming a well-rounded full-stack developer.
     </StyledTypography>
     <StyledTypography>
-      Most recently, I have been strengthening my knowledge of Cloud
-      technologies and have achieved three certifications for Amazon Web
-      Services, while studying to obtain a fourth. I deployed this Website and
-      all linked past applications (originally deployed using Heroku) entirely
-      on AWS, which gives me more control and understanding of the
-      infrastructure required to create and deploy highly-available applications
-      and services.
+      In 2022, I created this website, backed by a fleet of personal projects to
+      showcase my skills. At the time, I had also obtained three certifications
+      from Amazon Web Services. I applied to software development jobs and a
+      few weeks later, was a professional developer!
+    </StyledTypography>
+    
+    <StyledTypography>
+      Almost two years later, I remain in the field and consider the switch one
+      of the best decisions I ever made. I enjoy my work and craft so much that
+      I often look forward to the week ahead. I have not stopped learning, and
+      am regularly learning new concepts, building projects, studying for more
+      certifications, or just trying out new technologies.
+    </StyledTypography>
+    
+    <StyledTypography>
+      Since I began in this field, I've learned and strengthened my abilities
+      when it comes to things such as AWS, Kubernetes, learning new programming
+      languages, and developer productivity.
+    </StyledTypography>
+    
+    <StyledTypography>
+      Today, I consider myself a motivated and passionate full-stack software
+      developer that is always looking for the next way to improve. When I'm not
+      outside or spending time with family and friends, I'm often building my
+      skills further or working on a project that I'm passionate about.
     </StyledTypography>
   </>
 )
@@ -57,61 +69,65 @@ export const accordionContentBackground = (
 export const accordionContentSkills = (
   <>
     <StyledTypography>
-      In university, I learned multiple programming languages such as C, C++,
-      Python, and Perl. After learning the common patterns of programming with
-      lower-level languages, I then moved on to develop my knowledge of Python
-      and R. I learned even more at this point, having a bevy of new challenges
-      to conquer in the workflows of data analysis.
+      I am proficient in a few programming languages, as well technologies that
+      I've learned about and used as my curiosity leads me and I have new
+      requirements for my projects.
     </StyledTypography>
+
     <StyledTypography>
-      When I began to focus more of my time on Web application development, I
-      started with Django, a Python-based framework for designing Web
-      applications. Django taught me about the Model-View-Controller model, Web
-      request/responses, and solidified my understanding of the basics of Web
-      design: HTML, CSS, and vanilla JavaScript. I also became proficient with
-      JQuery, even though I may never use it again…
+      I am very comfortable writing TypeScript and JavaScript code both on the
+      front-end and the back-end. While I have worked in plain JavaScript
+      projects, I really enjoy the information that TypeScript can provide to
+      me in my IDE.
     </StyledTypography>
+    
     <StyledTypography>
-      After designing numerous Web applications with Django, I began to seek
-      more learning of TypeScript/JavaScript/JSX and dove into the world of
-      React. I took a few Udemy courses to learn best practices and began
-      applying my knowledge by building new applications and even re-building
-      previous projects of mine with my new toolset. I learned so much about
-      implementing authentication/authorization, caching, React’s declarative
-      style, and I grew to appreciate the technology very much. It is now my
-      primary tool for developing Web applications.
+      I'm also proficient with Python, having used it professionally, as well
+      as in projects involving Django, Pandas/Numpy, Tkinter, and more.
     </StyledTypography>
+    
     <StyledTypography>
-      A few other technologies/concepts stood out as necessary to learn, so I
-      did just that. I started to use Docker and the principle of test-driven
-      development in my applications, and saw their powers for deployment and
-      building robustness into my applications.
+      My favorite language of all is Rust. Having started to work with it in
+      2022, I quickly fell in love. Its amazing type system, memory guarantees,
+      and developer experience have me always looking for excuses to write more.
+      I now use it for all of my personal projects. On the back-end I typically
+      deploy a serverless REST API with API Gateway and Rust Lambda functions.
+      On the front-end, I've started to play around with the Yew framework
+      (similar to React), which allows me to build blazingly fast UIs without
+      any garbage collection.
     </StyledTypography>
+      
     <StyledTypography>
-      Other skills that I possess but did not mention above include:
+      Front-end technologies I've used:
+      <ul>
+        <li>React</li>
+        <li>Vue</li>
+        <li>Yew</li>
+      </ul>
     </StyledTypography>
-    <ul>
-      <StyledListItem style={{ marginTop: '10px' }}>
-        <StyledTypography style={{ textIndent: '0px' }}>
-          SQL and relational/non-relational database design
-        </StyledTypography>
-      </StyledListItem>
-      <StyledListItem>
-        <StyledTypography style={{ textIndent: '0px' }}>
-          Git, version control systems, and virtual environments
-        </StyledTypography>
-      </StyledListItem>
-      <StyledListItem>
-        <StyledTypography style={{ textIndent: '0px' }}>
-          Machine learning models (regression, classifiers, clustering, etc.)
-        </StyledTypography>
-      </StyledListItem>
-      <StyledListItem>
-        <StyledTypography style={{ textIndent: '0px' }}>
-          Strong communication, presentation, and team-building skills
-        </StyledTypography>
-      </StyledListItem>
-    </ul>
+    
+    <StyledTypography>
+      As I've gained experience and learned more about AWS, my interest in DevOps has grown. This motivated me to learn more about technologies such as:
+      <ul>
+        <li>Kubernetes</li>
+        <li>Terraform</li>
+        <li>AWS CloudFormation</li>
+        <li>CI/CD</li>
+        <li>shell scripting</li>
+      </ul>
+    </StyledTypography>
+    
+    <StyledTypography>
+      Other skills:
+      <ul>
+        <li>SQL and relational database design, including PostgreSQL, MySQL, Amazon RDS, and Oracle</li>
+        <li>NoSQL databases including DynamoDB and MongoDB</li>
+        <li>Git, version control systems</li>
+        <li>Docker and Kubernetes</li>
+        <li>Understanding of data structures and algorithms</li>
+        <li>Strong communication, presentation, and team-building skills</li>
+      </ul>
+    </StyledTypography>
   </>
 )
 
@@ -123,26 +139,24 @@ export const accordionContentHowSiteWasBuilt = (
       control that I wanted, nor did I fully understand what was actually
       necessary for deployment.
     </StyledTypography>
+
     <StyledTypography>
-      It became clear to me that Amazon Web Services (AWS) was the solution.
-      There are so many services within, and many well-known companies use them
-      for their business needs. I took courses to learn about the vast ecosystem
-      and studied for three of the examinations, all of which I passed. I then
-      began to plan and design my architecture for this very site and my other
-      hosted applications.
+      It became clear to me that Amazon Web Services (AWS) was the solution. There
+      are so many services within, and many large companies use them for their
+      business needs. I took courses to learn about the vast ecosystem and studied
+      for their certifications. I then began to plan and design my architecture
+      for this very site and my other hosted applications.
     </StyledTypography>
+
     <StyledTypography>
       I had to make many design decisions including whether to use Elastic
       Container Service (ECS) vs. Elastic Beanstalk vs. EC2, how I wanted to
-      configure routing from/to all of my services, security, and TLS
-      certificates.
-      <i>
-        Was a load balancer necessary? Should I deploy the front-end through
-        Amazon S3, or Docker-ize that also?
-      </i>
-      There were so many things I learned by applying the knowledge I gained
-      from studying for the examinations.
+      configure routing from/to all of my services, security, and TLS certificates.
+      <i>Was a load balancer necessary? Should I deploy the front-end through Amazon
+      S3, or Docker-ize that also?</i> There were so many things I learned by
+      applying the knowledge I gained from studying for the examinations.
     </StyledTypography>
+
     <StyledTypography>
       The final step was to convert all of this design into Infrastructure as
       Code. After learning about/working in AWS, it quickly became clear to me
@@ -152,47 +166,39 @@ export const accordionContentHowSiteWasBuilt = (
       templates that built everything from the security groups to the CloudFront
       distributions.
     </StyledTypography>
+
     <StyledTypography>
-      I ended up with different stacks based on the type of service being
-      deployed. The front-end of my React applications were habituated in an S3
-      bucket, while the back-end API was Docker-ized in ECS Fargate. For past
-      Django projects, a different stack was required, and I ended up creating
-      an entirely automated CodePipeline workflow. It would take a CodeCommit
-      repository and generate Docker images from the Dockerfiles to be stored in
-      Elastic Container Registry (ECR). It would then run them through a
+      I ended up with different stacks based on the type of service being deployed.
+      The front-end of my React applications were habituated in an S3 bucket,
+      while the back-end API was Docker-ized in ECS Fargate. For past Django
+      projects, a different stack was required, and I ended up creating an
+      entirely automated CodePipeline workflow. It would take a CodeCommit
+      repository and generate Docker images from the Dockerfiles to be stored
+      in Elastic Container Registry (ECR). It would then run them through a
       pipeline with a `docker-compose.yml` file that along with ECS
       `docker-convert` turns the Docker instructions into CloudFormation code,
       which is then applied as a stack! I also have services that exist solely
       as an API, so a specific solution was implemented in these scenarios.
     </StyledTypography>
+
     <StyledTypography>
       Some examples of the AWS technologies I used:
-    </StyledTypography>
-    <div>
       <ul>
-        <StyledListItem style={{ marginTop: '10px' }}>
-          <StyledTypography style={{ textIndent: '0px' }}>
-            CloudFront, for Distributions that pointed to S3 buckets and
-            ECS-hosted APIs
-          </StyledTypography>
-        </StyledListItem>
-        <StyledListItem>
-          <StyledTypography style={{ textIndent: '0px' }}>
-            Amazon Certificate Manager (ACM), to create TLS certificates to
-            implement security on my Distributions and Load Balancers
-          </StyledTypography>
-        </StyledListItem>
-        <StyledListItem>
-          <StyledTypography style={{ textIndent: '0px' }}>
-            Amazon Virtual Private Cloud (VPC), to create an isolated
-            environment for some of my services to live
-          </StyledTypography>
-        </StyledListItem>
+        <li>CloudFront, for Distributions that pointed to S3 buckets and ECS-hosted APIs</li>
+        <li>
+          Amazon Certificate Manager (ACM), to create TLS certificates to
+          implement security on my Distributions and Load Balancers
+        </li>
+        <li>
+          Amazon Virtual Private Cloud (VPC), to create an isolated
+          environment for some of my services to live
+        </li>
       </ul>
-    </div>
+    </StyledTypography>
+
     <StyledTypography>
-      Please see the Portfolio section below, where I link to a GitHub
-      repository that contains all of the CloudFormation code that I created.
+      Please see the Portfolio section below, where I link to a GitHub repository
+      that contains all of the CloudFormation code that I created.
     </StyledTypography>
   </>
 )
