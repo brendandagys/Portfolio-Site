4c41c77a1d40b89b68da3150a193b21ee6680e63
Brendan Dagys <brendandagys@gmail.com>
2023-06-15 20:20:12 -0400

    Update homepage title and add box shadow

--- frontend/src/components/HomepageTitles.tsx
+++ frontend/src/components/HomepageTitles.tsx
@@ -13,9 +13,9 @@ const HomepageTitles = ({ theme }: { theme: Theme }): React.ReactElement => {
           background:
             'linear-gradient(90deg, rgba(0, 219, 222, 0.2) 0%, rgba(252, 0, 255, 0.2) 100%)',
           maxWidth: '550px',
-          // width: '95%',
           margin: 'auto',
           borderRadius,
+          boxShadow: '2px 2px 50px 2px #d3d3d3'
         }}
       >
         <Typography
@@ -23,25 +23,26 @@ const HomepageTitles = ({ theme }: { theme: Theme }): React.ReactElement => {
           align='center'
           mt='6.75rem'
           px={7}
-          py={1}
+          py={2}
           sx={{
-            color: theme.palette.colorMode.titleTextColor,
-            backgroundColor: 'rgba(255, 255, 255, .15)',
-            backdropFilter: 'blur(12px)',
             WebkitBackdropFilter: 'blur(12px)',
+            backdropFilter: 'blur(12px)',
+            backgroundColor: 'rgba(255, 255, 255, .15)',
+            borderRadius,
+            color: theme.palette.colorMode.titleTextColor,
+            fontWeight: 'bold',
             maxWidth: '550px',
             mx: 'auto',
-            borderRadius,
-            fontWeight: 'bold',
           }}
         >
-          TypeScript and Python developer
+          Full-stack TypeScript, Python, and Rust software developer
         </Typography>
       </div>
       <Typography
         align='center'
         variant='h4'
-        py='30px'
+        pb='20px'
+        pt='60px'
         px={4}
         sx={{ fontWeight: 'light', color: theme.palette.fontColor.secondary }}
       >
