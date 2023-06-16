6b20d00f882b4815cd836662481578fd720ef737
Brendan Dagys <brendandagys@gmail.com>
2023-06-15 20:19:49 -0400

    Update and underline homepage 'Who I am' section

--- frontend/src/components/HomepageDescription.tsx
+++ frontend/src/components/HomepageDescription.tsx
@@ -25,28 +25,29 @@ const HomepageDescription = (): React.ReactElement => {
         mb={5}
         sx={{
           fontFamily: 'Courier New',
-          // textDecoration: 'underline 2.25px',
+          textDecoration: 'underline 2.25px',
           textShadow: '3px 3px 2px gray',
         }}
       >
         Who I am
       </StyledTypography>
       <StyledTypography px={6} fontWeight='light' variant='subtitle1' paragraph>
-        I am a passionate developer with a strong foundation in
-        TypeScript/JavaScript, Python, and other languages. I have experience in
-        analytics as well as with Cloud services and technologies, mainly AWS.
+        I am a passionate developer that is comfortable in TypeScript/JavaScript,
+        Python, Rust, and other languages. I have professional experience with
+        Amazon Web Services, and deploy my own solutions on the platform, usually
+        with a mix of CloudFormation, SAM, S3, and CloudFront, to name a few.
       </StyledTypography>
       <StyledTypography px={6} fontWeight='light' variant='subtitle1' paragraph>
         I love to solve problems and to build things. I have professional
-        experience creating and designing software solutions through
-        object-oriented as well as procedural programming. In my spare time you
-        can find me outside biking, playing guitar, and working on numerous
-        personal projects to further my interest and knowledge.
+        experience creating and designing software solutions with various
+        programming paradigms and technologies. In my spare time you can find
+        me outside biking, playing guitar, and working on numerous personal
+        projects to further my interest and knowledge.
       </StyledTypography>
       <StyledTypography px={6} fontWeight='light' variant='subtitle1' paragraph>
         Thanks for visiting my site and reading this far! Below is a more
-        detailed description about myself, and some examples of projects I've
-        created. Feel free to contact me at the bottom of the page!
+        detailed description about myself, and a few of my projects. Feel free
+        to contact me at the bottom of the page!
       </StyledTypography>
     </StyledDiv>
   )
